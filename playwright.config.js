88fd460d54047bd48906a4c4305984dc705e7494
Matthew Ling <matthewling@stripe.com>
2022-08-29 12:16:11 +0100

    Install playwright

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { devices } = require('@playwright/test');
+const { devices } = require("@playwright/test");
 
 /**
  * Read environment variables from file.
@@ -7,13 +7,12 @@ const { devices } = require('@playwright/test');
  */
 // require('dotenv').config();
 
-
 /**
  * @see https://playwright.dev/docs/test-configuration
  * @type {import('@playwright/test').PlaywrightTestConfig}
  */
 const config = {
-  testDir: './tests',
+  testDir: "./tests",
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
@@ -21,7 +20,7 @@ const config = {
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 5000,
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -32,7 +31,7 @@ const config = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
@@ -41,30 +40,30 @@ const config = {
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
+      name: "chromium",
       use: {
-        ...devices['Desktop Chrome'],
+        ...devices["Desktop Chrome"],
       },
     },
 
     //{
-      //name: 'firefox',
-      //use: {
-        //...devices['Desktop Firefox'],
-      //},
+    //name: 'firefox',
+    //use: {
+    //...devices['Desktop Firefox'],
+    //},
     //},
 
     //{
-      //name: 'webkit',
-      //use: {
-        //...devices['Desktop Safari'],
-      //},
+    //name: 'webkit',
+    //use: {
+    //...devices['Desktop Safari'],
+    //},
     //},
 
     /* Test against mobile viewports. */
@@ -101,8 +100,8 @@ const config = {
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'node -r dotenv/config server.js dotenv_config_path=../.env',
-    port: 3000,
+    command: "npm run start",
+    port: 4242,
   },
 };
 
